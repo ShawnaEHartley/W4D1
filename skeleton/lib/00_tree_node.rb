ba6d326876ae10de3513d84e08efcb531468f8fe
“Shawna <shawna.e.hartley@gmail.com>
2022-11-28 14:21:59 -0500

    progress on tree node

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,24 @@
 class PolyTreeNode
 
+    def initialize(value)
+        @parent = nil
+        @children = []
+        @value = value
+    end
+
+    attr_reader :parent, :children, :value
+
+    #node1 = root
+    #node2 = inernal (child of node 1 & a parent)
+    # node2.parent => node1
+    def parent=(new_parent)
+        if !new_parent.children.include?(self) && self.parent != nil
+            @parent = new_parent
+            self.parent.children.push(self)
+
+        end
+
+
+    end
+
 end
